4b99aab26fdd7070a5f31776889dbb5c7948b7c5
Dominik Kowalczyk <xkowal99@gmail.com>
2020-06-23 21:46:06 +0200

    json fix

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -21,8 +21,18 @@ void Settings::save() {
     Log::log(Log::INFO, "[****] Saving settings.\n");
     std::ofstream dataFile("data/settings.json");
     dataFile.clear();
-
-    dataFile << data.dump(4);
+    
+    std::string res = data.dump(4);
+    int array = 0;
+    for(int i = 0; i < (int)res.size(); i++) {
+        if(res[i] == '[') array++;
+        if(res[i] == ']') array--;
+        if(array > 0 and ((res[i] == '\n' or res[i] == '\t') or (res[i] == ' ' and res[i - 1] == ' '))) {
+            res.erase(i, 1);
+            i--;
+        }
+    }
+    dataFile << res;
     
     dataFile.close();
     Log::log(Log::INFO, "[DONE] Saving settings.\n");

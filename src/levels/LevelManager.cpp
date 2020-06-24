9735334369db651c0d6e779d79a6424ece266d9d
Dominik Kowalczyk <xkowal99@gmail.com>
2020-06-24 22:16:43 +0200

    fix waves

--- src/levels/LevelManager.cpp
+++ src/levels/LevelManager.cpp
@@ -48,6 +48,62 @@ void LevelManager::load() {
         std::vector<std::shared_ptr<BaseEnemy>> enemies;
         uint enemy_id = 0;
 
+        std::map<std::pair<float, float>, std::vector<sf::Vector2f>> best_formation_pos;
+
+        {
+            std::vector<std::pair<sf::Vector2f, int>> temp;
+            uint cnt = 0;
+            
+            for(auto& [enemy_number, enemy] : args["enemies"].items()) {
+                auto X = enemy.at("X").get<std::vector<float>>();
+                auto Y = enemy.at("Y").get<std::vector<float>>();
+                auto count = enemy.at("count").get<uint>();
+                cnt += count;
+                auto x = X.back(), y = Y.back();
+                temp.push_back({{x, y}, count});
+            }
+            decltype(formation_temp) form;
+
+            uint roz_size = 8;
+            while(form.size() < cnt) {
+                uint roz = form.size() + roz_size;
+                roz_size = 15 - roz_size;
+                roz = std::min((uint)formation_temp.size(), roz);
+                form = std::vector<sf::Vector2f>(formation_temp.begin(), formation_temp.begin() + roz);
+            }
+            
+            while(cnt--) {
+                float dbest = 1e9;
+                for(auto& [p, c] : temp) {
+                    if(not c) continue;
+                    for(auto& p2 : form) {
+                        auto d = (p.x - p2.x) * (p.x - p2.x) + (p.y - p2.y) * (p.y - p2.y);
+                        dbest = std::min(dbest, d);
+                    }
+                }
+                bool g = 0;
+                for(auto& [p, c] : temp) {
+                    if(not c) continue;
+                    for(auto& p2 : form) {
+                        auto d = (p.x - p2.x) * (p.x - p2.x) + (p.y - p2.y) * (p.y - p2.y);
+                        if(abs(d - dbest) <= 0.1f) {
+                            g = 1;
+                            Log::log(Log::INFO, "best = {} {}, {} {}\n", p.x, p.y, p2.x, p2.y);
+                            best_formation_pos[{p.x, p.y}].push_back(p2);
+                            form.erase(std::find(form.begin(), form.end(), p2));
+                            c--;
+                            break;
+                        }
+                    }
+                    if(g) break;
+                }
+
+                if(not g) {
+                    Log::log(Log::INFO, "GOWNO {}\n", dbest);
+                }
+            }
+        }
+
         for(auto& [enemy_number, enemy] : args["enemies"].items()) {
             Log::log(Log::INFO, "enemy = {}\n", enemy_number);
             
@@ -68,14 +124,9 @@ void LevelManager::load() {
             if(type == "small") {
                 for(uint i = 0; i < count; i++) {
                     auto last_pos = goals.back();
-                    auto it = std::min_element(formation.begin(), formation.end(), [last_pos](auto p1, auto p2) {
-                        auto d = [last_pos](auto p) {
-                            return (p.x - last_pos.x) * (p.x - last_pos.x) + (p.y - last_pos.y) * (p.y - last_pos.y);
-                        };
-                        return d(p1) < d(p2);
-                    });
-                    goals.push_back(*it);
-                    formation.erase(it);
+                    Log::log(Log::INFO, "en = {} {}\n", last_pos.x, last_pos.y);
+                    goals.push_back(best_formation_pos[{last_pos.x, last_pos.y}][0]);
+                    best_formation_pos[{last_pos.x, last_pos.y}].erase(best_formation_pos[{last_pos.x, last_pos.y}].begin());
                     enemy_id++;
                     enemies.emplace_back(new SmallEnemy(goals, time_offset_start + time_offset * i, speed, start_rotation, m_times_played * enemy_hp, texture_path));
                     goals.pop_back();
